209cd22c8cc23740921f2f347272f0be7a338684
romeonicholas <nicholas.romeo@code.berlin>
2024-11-12 11:55:33 +0100

    Move wifi function to setup file, add additional notifications

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-from machine import Pin
-from utime import sleep
-import network
+import machine
+import utime
 import urequests
 
+import setup
 import secrets
 
-pin = Pin("LED", Pin.OUT)
+pin = machine.Pin("LED", machine.Pin.OUT)
 
-
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect(secrets.SSID, secrets.PASSWORD)
+setup.connect_to_wifi(secrets.SSID, secrets.PASSWORD)
 
 cat_fact_req = urequests.get("http://cat-fact.herokuapp.com/facts")
 cat_fact = cat_fact_req.json()
@@ -20,7 +17,7 @@ print("LED starts flashing...")
 while True:
     try:
         pin.toggle()
-        sleep(1)  # sleep 1sec
+        utime.sleep(1)  # sleep 1sec
     except KeyboardInterrupt:
         break
 pin.off()

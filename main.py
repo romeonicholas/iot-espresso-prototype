73b23a767a1b8c845d3c2111fdb283c5da626aa8
romeonicholas <nicholas.romeo@code.berlin>
2024-11-12 16:07:59 +0100

    Remove demo code, add setup mode web server with get routes

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
-import machine
-import utime
-import urequests
+import phew
+import phew.server
+import phew.dns
 
-import setup
 import secrets
 
-pin = machine.Pin("LED", machine.Pin.OUT)
+ACCESS_POINT_NAME = "IoT Espresso Assistant"
+ACCESS_POINT_DOMAIN = "iot.espresso.net"
 
-setup.connect_to_wifi(secrets.SSID, secrets.PASSWORD)
+# TODO: connect to wifi after setup
+# setup.connect_to_wifi(secrets.SSID, secrets.PASSWORD)
 
-cat_fact_req = urequests.get("http://cat-fact.herokuapp.com/facts")
-cat_fact = cat_fact_req.json()
-print(cat_fact)
+def setup_mode():
+    print("Entering setup mode...")
 
-print("LED starts flashing...")
-while True:
-    try:
-        pin.toggle()
-        utime.sleep(1)  # sleep 1sec
-    except KeyboardInterrupt:
-        break
-pin.off()
-print("Finished.")
+    @phew.server.route("/", methods=['GET'])
+    def index():
+        return "Hello, World!", 200
+
+    @phew.server.route("/wrong-host-redirect", methods=['GET'])
+    def wrong_host():
+        body = "<!DOCTYPE html><head><meta http-equiv='refresh' content='0;URL=http://" + ACCESS_POINT_DOMAIN + "' /></head>"
+        return body
+
+    @phew.server.route("/hotspot-detect.html", methods=["GET"])
+    def hotspot():
+        return "This is the portal", 200
+
+    @phew.server.catchall()
+    def catch_all(request):
+        if request.headers.get("host") != ACCESS_POINT_DOMAIN:
+            return phew.server.redirect("http://" + ACCESS_POINT_DOMAIN + "/wrong-host-redirect")
+        return None
+
+    ap = phew.access_point(ACCESS_POINT_NAME)
+    ip = ap.ifconfig()[0]
+    phew.dns.run_catchall(ip)
+
+
+setup_mode()
+phew.server.run()

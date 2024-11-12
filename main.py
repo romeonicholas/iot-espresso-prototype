1c4c527c5894d2b77c151439466408087805273c
romeonicholas <nicholas.romeo@code.berlin>
2024-11-12 18:05:17 +0100

    Add post route to write network config

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+import os
+import json
+import machine
+import utime 
+
 from phew import server, access_point, dns
 from phew.template import render_template
 
-import secrets
-
 ACCESS_POINT_NAME = "IoT Espresso Assistant"
 ACCESS_POINT_DOMAIN = "iot.espresso.net"
+CONFIG_FILE = "config.json"
 
-# TODO: connect to wifi after setup
-# setup.connect_to_wifi(secrets.SSID, secrets.PASSWORD)
 
 def setup_mode():
     print("Entering setup mode...")
@@ -19,7 +21,19 @@ def setup_mode():
 
         return render_template("templates/index.html")
 
-    
+    @server.route("/setup", methods=['POST'])
+    def setup(request):
+        ssid = request.form.get("ssid")
+
+        if not ssid:
+            return "SSID is required", 400
+
+        with open(CONFIG_FILE, "w", encoding="utf-8") as f:
+            json.dump(request.form, f)
+            f.close()
+
+        return render_template("templates/index.html")
+
     @server.catchall()
     def catch_all(request):
         if request.headers.get("host") != ACCESS_POINT_DOMAIN:
@@ -31,6 +45,19 @@ def setup_mode():
     ip = ap.ifconfig()[0]
     dns.run_catchall(ip)
 
+try:
+    os.stat(CONFIG_FILE)
 
-setup_mode()
+    with open(CONFIG_FILE, "r", encoding="utf-8") as f:
+        wifi_credentials = json.load(f)
+
+        print("Trying to connec to network...")
+        # TODO: connect to wifi after setup
+        # setup.connect_to_wifi(config.SSID, config.PASSWORD)
+
+except Exception:
+    print("Couldn't connect to network, entering setup mode...")
+    setup_mode()
+
+
 server.run()
